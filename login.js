02c4c3c82c3f0cd70f4455cd96e6ca5153ce7325
Shruti0520 <65389795+Shruti0520@users.noreply.github.com>
2021-07-21 16:19:43 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,12 @@
+function required() {
+  var user = document.form1.text1.value;
+  var pass = document.form1.text2.value;
+
+
+    if(user =="" || pass =="") {
+    return false;
+  }
+  document.form1.submit;
+  return true;
+
+}
